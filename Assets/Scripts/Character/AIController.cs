61df1cdcdd8f8b5dd1e9a2c29a87ea2961b708d1
robertBeattie <rbeattie@uvm.edu>
2021-11-16 12:26:44 -0500

    mob check, spelling, comments

--- Assets/Scripts/Character/AIController.cs
+++ Assets/Scripts/Character/AIController.cs
@@ -5,10 +5,34 @@ using UnityEngine;
 [RequireComponent(typeof(Movement))]
 public class AIController : MonoBehaviour
 {
-	enum Behavior { Passive, Netraul, Aggressive };
-	[SerializeField] Behavior behavior;
-	enum MovementBehavior { Idle, Wander, Patrol, Follow, Persue, Distance };
+	/*
+		Passive does not fight back
+		Neutral fights back once provoked
+		Aggressive fights with out being provoked 
+	*/
+	enum AggressionBehavior { Passive, Neutral, Aggressive };
+	[SerializeField] AggressionBehavior aggressionBehavior;
+
+	/**
+		Idle stand around
+		Wander randomly move around home postion by some radius
+		Patrol move betweem points in a list
+		Follow follow behind Transform by some distance
+		Persue Melee attacker, run after Transform
+		Flee Run from Transform
+		Distance Ranged attacker, trying to keep distance from Transform
+	*/
+	enum MovementBehavior { Idle, Wander, Patrol, Follow, Persue, Flee, Distance };
 	[SerializeField] MovementBehavior movementBehavior;
+
+	/*
+		Solo does not call for help from other characters around them
+		Local calls for help from characters near them in some range
+		Global calls for help from characters not local to them making them aggressive 
+			when a user leaves the scene
+	*/
+	enum MobBehavior {Solo, Local, Global};
+	[SerializeField] MobBehavior mobBehavior;
 
 	Movement movement;
 	private void Awake() {
@@ -21,14 +45,14 @@ public class AIController : MonoBehaviour
     }
 
 	void ActionBehavior() {
-		switch (behavior) {
-			case Behavior.Passive:
+		switch (aggressionBehavior) {
+			case AggressionBehavior.Passive:
 				Debug.Log("0");
 				break;
-			case Behavior.Netraul:
+			case AggressionBehavior.Neutral:
 				Debug.Log("1");
 				break;
-			case Behavior.Aggressive:
+			case AggressionBehavior.Aggressive:
 				Debug.Log("2");
 				break;
 		}
