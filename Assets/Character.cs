11026df5416ead946cb9e9a683535fe71d3bacbd
Robert Beattie <50488761+robertBeattie@users.noreply.github.com>
2021-12-07 17:32:15 -0500

    stats update and ui

--- Assets/Character.cs
+++ Assets/Character.cs
@@ -6,16 +6,19 @@ public class Character : MonoBehaviour
 {
     public Stats stats;
 
+    private int STAMINA_TO_HEALTH_MOD = 5;
+    private int WILL_TO_MANA_MOD = 10;
+
+
     public int xp;
     [SerializeField] public int health;
     public int maxHealth;
     [SerializeField] public int mana; 
-    public int maxMana; 
+    public int maxMana;
+    
     public int attPwr = 0;
 
-
     public int stamina = 0;
-
 
     public int will = 0;
 
@@ -23,11 +26,36 @@ public class Character : MonoBehaviour
 
     public int magicDef = 0;
 
-    public float critDmg = 1.75f;
+    public float critDmg = 0;
 
-    public int crit = 100; 
+    public int crit = 0; 
 
     public int haste = 0; 
 
-    public int evade = 0;   
+    public int evade = 0;
+
+	private void Awake() {
+        ReadinStats();
+
+    }
+
+
+    private void ReadinStats() {
+        xp = stats.xp;
+
+        maxHealth = stats.stamina * STAMINA_TO_HEALTH_MOD;
+        health = maxHealth;
+        maxMana = stats.will * WILL_TO_MANA_MOD;
+        mana = maxMana;
+
+        attPwr = stats.attPwr;
+        physDef = stats.physDef;
+        magicDef = stats.magicDef;
+
+        critDmg = stats.critDmg;
+        crit = stats.crit;
+        haste = stats.haste;
+        evade = stats.evade;
+
+    }
 }
--- Assets/Scripts/Character/Stats.cs
+++ Assets/Scripts/Character/Stats.cs
@@ -5,24 +5,29 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "Stats Default", menuName = "CharacterStats/StatsObject", order = 1)]
 public class Stats : ScriptableObject
 {  
+    //xperience
     public int xp;
 
-    //Atk Pwr - Damage, Heals, Damage Sheilds
-    public int AttPwr = 0;
-    //Will -every point = 10mana
-     public int Stamina = 0;
-    //Phys Def reduces Phy damage by this much
-    public int Will = 0;
     //Stamina - every point = 5 health
-    public int PhysDef = 0;
+    public int stamina = 10;
+    //Will -every point = 10mana
+    public int will = 10;
+
+
+    //Atk Pwr - Damage, Heals, Damage Sheilds
+    public int attPwr = 10;
+    //Phys Def reduces Phy damage by this much
+    public int physDef = 0;
     //Magic Def reduces Magic damage by this much
-    public int MagicDef = 0;
+    public int magicDef = 0;
+
+
     //Crit Dmg damage scale of critical attacks base 175%
-    public float CritDmg = 1.75f;
+    public float critDmg = 1.75f;
     //Crit = 20 is 1% crit chance
-    public int Crit = 100; // 5% base
+    public int crit = 100; // 5% base
     //Haste = 20 is 1% casting time reduction
-    public int Haste = 0; 
+    public int haste = 0; 
     //Evade = 20 is 1% chance to dodge 
-    public int Evade = 0;
+    public int evade = 0;
 }
